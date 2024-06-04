ffcafbadc049f7a728ff83fe5dd9252fc90db619
albim72 <marcin.albiniak72@gmail.com>
2024-06-04 12:24:41 +0200

    Create main.py

---
+++ DZIEN_1/moje_moduly/main.py
@@ -0,0 +1,13 @@
+# import dane
+# import dane as dn
+from dane import nrfilii as nf
+from dane import book as bk
+from moje_funkcje.fkolekcje import czytaj_liste,czytaj_slownik
+
+print(nf)
+print(bk)
+
+print("_"*50)
+czytaj_liste(nf)
+czytaj_slownik(bk)
+
